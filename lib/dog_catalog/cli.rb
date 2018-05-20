6afad359580f4ab82e5d3a731794e3f746db2f1d
Trac Nguyen <nguyendtrac@gmail.com>
2018-05-20 07:51:53 +0000

    updated cli.rb

--- lib/dog_catalog/cli.rb
+++ lib/dog_catalog/cli.rb
@@ -1,15 +1,21 @@
 class DogCatalog::CLI
 
   def call
-    puts "Welcome to the catalog, which dog supplies would you like to view:"
     list_categories
+    menu
   end
 
   def list_categories
+    puts "Welcome to the catalog!"
     puts <<-DOC.gsub /^\s*/, ''
     1. Leashes
     2. Collars
     3. Muzzles
     DOC
+  end
+
+  def menu
+    puts "Enter the number of the category you'd like to view:"
+    input = gets.strip
   end
 end
