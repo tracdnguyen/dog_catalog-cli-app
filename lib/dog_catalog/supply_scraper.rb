706ea7a10553e43c431d2628489c2a46507f8086
Trac Nguyen <nguyendtrac@gmail.com>
2018-05-25 16:48:55 +0000

    updated variable names to reflect application more

--- lib/dog_catalog/supply_scraper.rb
+++ lib/dog_catalog/supply_scraper.rb
@@ -31,8 +31,8 @@ class DogCatalog::SupplyScraper
   end
 
     def initialize
-      supplies_2 = [[],[],[]]
-      supplies_3 = [[],[],[]]
+      flat_collar_name_url_price = [[],[],[]]
+      prong_collar_name_url_price = [[],[],[]]
       name_url_price = [[],[],[]]
 
       collar_website = [
@@ -54,42 +54,40 @@ class DogCatalog::SupplyScraper
       flat_collars = Nokogiri::HTML(open("http://leerburg.com/flatcollars.htm"))
       flat_collars.css("div#container div#category-container div#category-list-container table tr td div table tr td a.itemname").each do |supply|
     #   supply.attributes["href"].value retreives links, but contain duplicates
-        supplies_2[0] << supply.text
-        supplies_2[0].delete("") #removes empty elements from array
-        supplies_2[1] << supply.attributes["href"].value
-        supplies_2[1].uniq!
+        flat_collar_name_url_price[0] << supply.text
+        flat_collar_name_url_price[0].delete("") #removes empty elements from array
+        flat_collar_name_url_price[1] << supply.attributes["href"].value
+        flat_collar_name_url_price[1].uniq!
       end
       flat_collars.css("div#container div#category-container div#category-list-container table tr td div.category-product-tile table tr td div.redtext span").each do |price|
-        supplies_2[2] << price.text
-        supplies_2[2].delete("$39.99") #original price marked out
-        supplies_2[2].delete("$14.99 - $14.99") #original price marked out
-        supplies_2[2].delete("$19.99") #original price marked out
+        flat_collar_name_url_price[2] << price.text
+        flat_collar_name_url_price[2].delete("$39.99") #original price marked out
+        flat_collar_name_url_price[2].delete("$14.99 - $14.99") #original price marked out
+        flat_collar_name_url_price[2].delete("$19.99") #original price marked out
       end
 
       prong_collars = Nokogiri::HTML(open("http://leerburg.com/prongcollars.htm"))
       prong_collars.css("div#container div#category-container div#category-list-container table tr td div table tr td a.itemname").each do |supply|
       # supply.attributes["href"].value retreives links, but contain duplicates
-        supplies_3[0] << supply.text
-        supplies_3[0].delete("") #removes empty elements from array
-        supplies_3[0].uniq! #removes duplicate names, website contains duplicates
-        supplies_3[1] << supply.attributes["href"].value
-        supplies_3[1].uniq! #removes duplicate links
+        prong_collar_name_url_price[0] << supply.text
+        prong_collar_name_url_price[0].delete("") #removes empty elements from array
+        prong_collar_name_url_price[0].uniq! #removes duplicate names, website contains duplicates
+        prong_collar_name_url_price[1] << supply.attributes["href"].value
+        prong_collar_name_url_price[1].uniq! #removes duplicate links
       end
       prong_collars.css("div#container div#category-container div#category-list-container table tr td div.category-product-tile table tr td div.redtext span").each do |price|
-        supplies_3[2] << price.text
-        supplies_3[2].delete("$93.00 - $121.00") #removes price for a product with 2 prices
-        supplies_3[2].delete("$62.98 - $79.30") #removes price for a product with 2 prices
-        supplies_3[2].uniq! #code will break if website has same price or price range for 2 separate products
+        prong_collar_name_url_price[2] << price.text
+        prong_collar_name_url_price[2].delete("$93.00 - $121.00") #removes price for a product with 2 prices
+        prong_collar_name_url_price[2].delete("$62.98 - $79.30") #removes price for a product with 2 prices
+        prong_collar_name_url_price[2].uniq! #code will break if website has same price or price range for 2 separate products
       end
 
       test = name_url_price[1].each_slice(2).map(&:first)
       name_url_price[1] = test
-      name_url_price[0] << supplies_2[0]
-      name_url_price[1] << supplies_2[1]
-      name_url_price[2] << supplies_2[2]
-      name_url_price[0].flatten
-      name_url_price[1].flatten
-      name_url_price[2].flatten
+      name_url_price[0].concat flat_collar_name_url_price[0]
+      name_url_price[1].concat flat_collar_name_url_price[1]
+      name_url_price[2].concat flat_collar_name_url_price[2]
+
       binding.pry
     end
 
