37b090ae90850662f6604faa6bcd9cfd53e734a4
Trac Nguyen <nguyendtrac@gmail.com>
2018-05-25 23:09:11 +0000

    removed attr

--- lib/dog_catalog/supplies.rb
+++ lib/dog_catalog/supplies.rb
@@ -1,17 +1,10 @@
 class DogCatalog::Supplies
-  attr_accessor :name, :category, :price, :url
+  attr_accessor :name, :price, :url
 
-  @@all = []
 
   def initialize(name_url_price)
-    count = 0
-    while count <= name_url_price[0].length
-      self.name = name_url_price[0][count]
-      self.url = name_url_price[1][count]
-      self.price = name_url_price[2][count]
-      @@all << self
-      count += 1
-    end
+
+  end
   #  @@all << self
     binding.pry
   end
